a5009caf8cdca609cb1822cfc9ebdc0609db6c74
Kayode <120796380+devkayodehq@users.noreply.github.com>
2023-08-31 16:08:24 +0100

    First Commit

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import twitterIcon from './icon/twitter.avif';
+
+export const Footer = () => {
+  return (
+    <center>
+        <a href='https://twitter.com/devkayode'>
+        <img target='_blank' style={{width: "90%", marginTop: "50px", marginLeft: "150px"}} src={twitterIcon}/>
+        </a>
+        
+    </center>
+  
+  )
+}
